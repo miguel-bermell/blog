5d08dbfdecb065cfded984b5825aee0186547b9b
miguel-bermell <miguelbermell@gmail.com>
2024-04-19 19:51:49 +0200

    fix(): baseUrl prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseUrl: import.meta.env.MODE === 'development' ? 'http://localhost:5173' : 'https://bermell.dev',
+  baseUrl: import.meta.env.MODE === 'development' ? 'http://localhost:5173' : 'https://www.bermell.dev',
   production: import.meta.env.MODE !== 'development',
   apiUrl: import.meta.env['VITE_API_URL'],
   websiteUrl: import.meta.env['VITE_WEBSITE_URL'],
